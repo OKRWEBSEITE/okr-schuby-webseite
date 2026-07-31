d4bb68a64dd8f0b24bcec171b8845e1ee438119e
OKRWEBSEITE <okrschuby@gmail.com>
2026-07-31 23:18:05 +0200

    Pflegetool-Veröffentlichung: assets/infos-daten.js

--- assets/infos-daten.js
+++ assets/infos-daten.js
@@ -7,10 +7,5 @@
    ============================================================ */
 
 const INFOS = [
-  {
-    label: "Grußwort des 1. Vorsitzenden",
-    ueberschrift: "Liebe Schubyerinnen und Schubyer,",
-    text: "wir danken Ihnen für die zahlreichen Besuche unserer Veranstaltungen und unserer Weiterbildungsangebote. Nehmen Sie sich auch ein wenig Zeit, um sich unser aktuelles, abwechslungsreiches Programm anzuschauen — wir freuen uns auf Ihre Teilnahme!\n\nIch möchte mich außerdem bei den Organisatorinnen und Organisatoren von „Bürger im Gespräch\" bedanken. Ich freue mich, dass es gelungen ist, diese Veranstaltungsreihe wieder zu beleben.",
-    unterschrift: "Volker Hagge Ellhöft, 1. Vorsitzender"
-  }
+
 ];
