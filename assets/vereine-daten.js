3cacdb0d28250d378f15968518d1479aa38371ba
OKRWEBSEITE <okrschuby@gmail.com>
2026-07-31 23:56:05 +0200

    Pflegetool-Veröffentlichung: assets/vereine-daten.js

--- assets/vereine-daten.js
+++ assets/vereine-daten.js
@@ -10,5 +10,102 @@
    ============================================================ */
 
 const VEREINE = [
+  {
+    name: "Gemeinde Schuby",
+    ansprechpartner: [
+      { name: "Petra Schulze", funktion: "Bürgermeisterin", telefon: 304918, email: "", website: "https://www.schuby.de" }
+    ]
+  },
 
+  {
+    name: "Ev. Kirchengemeinde St. Michaelis Schuby",
+    ansprechpartner: [
+      { name: "Michael Herold", funktion: "Vorsitzender des Vereins", telefon: 4417, email: "", website: "" }
+    ]
+  },
+
+  {
+    name: "Freiwillige Feuerwehr Schuby",
+    ansprechpartner: [
+      { name: "Marco Kroymann-Meyer", funktion: "Wehrführer", telefon: "0179 3435666", email: "", website: "" }
+    ]
+  },
+
+  {
+    name: "DRK Ortsverein Schuby",
+    ansprechpartner: [
+      { name: "Janina Detlefsen", funktion: "Ansprechpartnerin", telefon: 990088, email: "", website: "" },
+      { name: "Catrin Haß", funktion: "Ansprechpartnerin", telefon: 948088, email: "", website: "" }
+    ]
+  },
+
+  {
+    name: "Schleswiger Husaren",
+    ansprechpartner: [
+      { name: "Sönke Matz", funktion: "Vorsitzender des Vereins", telefon: 4070, email: "", website: "" }
+    ]
+  },
+
+  {
+    name: "Männergesangverein Schuby",
+    ansprechpartner: [
+      { name: "Hans-Werner Moritz", funktion: "Vorsitzender des Vereins", telefon: 41462, email: "", website: "" }
+    ]
+  },
+
+  {
+    name: "Schubyer Reiterverein",
+    ansprechpartner: [
+      { name: "Tina Möller", funktion: "Vorsitzende des Vereins", telefon: 41222, email: "", website: "" }
+    ]
+  },
+
+  {
+    name: "Schleiblasorchester Schleswig e.V.",
+    ansprechpartner: [
+      { name: "Sönke Rades", funktion: "Vorsitzender des Vereins", telefon: "0171 2174401", email: "", website: "" }
+    ]
+  },
+
+  {
+    name: "Sportverein VfB Schuby",
+    ansprechpartner: [
+      { name: "Uwe Jürgensen", funktion: "Vorsitzender des Vereins", telefon: "04625 1895575", email: "", website: "" }
+    ]
+  },
+
+  {
+    name: "SSF Südschleswiger Verein, Bezirk Schuby",
+    ansprechpartner: [
+      { name: "Manuel Barz", funktion: "Vorsitzender des Vereins", telefon: "", email: "", website: "" }
+    ]
+  },
+
+  {
+    name: "Siedlergemeinschaft Schuby 1991",
+    ansprechpartner: [
+      { name: "Dietmar Dollase", funktion: "Vorsitzender des Vereins", telefon: 41057, email: "", website: "" }
+    ]
+  },
+
+  {
+    name: "Förderverein Grundschule Schuby",
+    ansprechpartner: [
+      { name: "Nicole Portius", funktion: "Vorsitzende des Vereins", telefon: "", email: "", website: "" }
+    ]
+  },
+
+  {
+    name: "Sozialverband Deutschland, Ortsverband Schuby",
+    ansprechpartner: [
+      { name: "Harald Müller", funktion: "Vorsitzender des Vereins", telefon: 9787742, email: "", website: "" }
+    ]
+  },
+
+  {
+    name: "Wildtierrettung Schuby e.V.",
+    ansprechpartner: [
+      { name: "Arne Hinrichsen", funktion: "Vorsitzender des Vereins", telefon: "0176 55922002", email: "", website: "" }
+    ]
+  }
 ];
