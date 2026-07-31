109ae438df2a0f414de2a7bc7423b8de138c99d6
OKRWEBSEITE <okrschuby@gmail.com>
2026-07-31 23:18:04 +0200

    Pflegetool-Veröffentlichung: assets/vorstand-daten.js

--- assets/vorstand-daten.js
+++ assets/vorstand-daten.js
@@ -8,11 +8,5 @@
    ============================================================ */
 
 const VORSTAND = [
-  { name: "Volker Hagge Ellhöft", funktion: "1. Vorsitzender", telefon: "949432" },
-  { name: "Anka Mees", funktion: "2. Vorsitzende", telefon: "0171 7053430" },
-  { name: "Heike Pöschel", funktion: "Kassenwart", telefon: "0151 42221931" },
-  { name: "Anneke Hinz", funktion: "Schriftwart", telefon: "0160 98778104" },
-  { name: "Detlef Scherer", funktion: "Beisitzer" },
-  { name: "Rolf Murst", funktion: "Beisitzer" },
-  { name: "Sina Schade", funktion: "Beisitzerin" }
+
 ];
