5d3437d4d8641be6a012806b9cf177c596661d9e
OKRWEBSEITE <okrschuby@gmail.com>
2026-08-01 22:38:29 +0200

    Pflegetool-Veröffentlichung: assets/vorstand-daten.js

--- assets/vorstand-daten.js
+++ assets/vorstand-daten.js
@@ -8,12 +8,12 @@
    ============================================================ */
 
 const VORSTAND = [
-  { name: "Volker Hagge Ellhöft", funktion: "1. Vorsitzender", telefon: 949432, email: "" },
+  { name: "Volker Hagge Ellhöft", funktion: "1. Vorsitzender", telefon: "949432", email: "" },
   { name: "Anka Mees", funktion: "2. Vorsitzende", telefon: "0171 7053430", email: "" },
-  { name: "Heike Pöschel", funktion: "Kassenwart", telefon: "0151 42221931", email: "" },
-  { name: "Anneke Hinz", funktion: "Schriftwart", telefon: "0160 98778104", email: "" },
-  { name: "Detlef Scherer", funktion: "Beisitzer", telefon: 41371, email: "" },
-  { name: "Rolf Murst", funktion: "Beisitzer", telefon: 41081, email: "" },
-  { name: "Sina Schade", funktion: "Beisitzerin", telefon: 1733112142, email: "" },
-  { name: "Michael Gerke", funktion: "Beisitzer", telefon: 1728173211, email: "" }
+  { name: "Heike Pöschel", funktion: "Kassenwartin", telefon: "0151 42221931", email: "heike@poeschel-schuby.de" },
+  { name: "Anneke Hinz", funktion: "Schriftwartin", telefon: "0160 98778104", email: "anneke.stallbaum@gmx.net" },
+  { name: "Detlef Scherer", funktion: "Beisitzer", telefon: "04621 41371", email: "scherer-detlef@gmx.de" },
+  { name: "Rolf Murst", funktion: "Beisitzer", telefon: "41081", email: "" },
+  { name: "Sina Schade", funktion: "Beisitzerin", telefon: "1733112142", email: "" },
+  { name: "Michael Gerke", funktion: "Beisitzer", telefon: "1728173211", email: "" }
 ];
