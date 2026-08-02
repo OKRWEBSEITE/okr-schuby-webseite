d34779d7cef5d85aa3c45f5fb45f694f0ccc7fe6
OKRWEBSEITE <okrschuby@gmail.com>
2026-08-02 21:55:02 +0200

    Pflegetool-Veröffentlichung: assets/daten.js

--- assets/daten.js
+++ assets/daten.js
@@ -185,15 +185,6 @@ const TERMINE = [
   },
 
   {
-    id: "OKR-2026-2-18", typ: "kurs", rubrik: "gesundheit",
-    titel: "Erste Hilfe für den Hund ( Mit dem eigenen Hund)",
-    beschreibung: "Handeln im Notfall ( Wundversorgung usw. alles im Notfall zu handel",
-    optionen: [
-      { bezeichnung: "", kursleiter: "Sarah Kreimendahl u. Katharina Bündel", start: "2026-09-27", wochentag: "Sonntag", uhrzeit: "10.00", ort: "Schuby Boyebüll 8", anzahl: 1, preis: 35, kursbegrenzung: 15 }
-    ]
-  },
-
-  {
     id: "OKR-2026-2-19", typ: "kurs", rubrik: "gesundheit",
     titel: "Erste Hilfe für den Hund ( Mit dem eigenen Hund)",
     beschreibung: "Handeln im Notfall ( Wundversorgung usw. alles im Notfall zu handel",
