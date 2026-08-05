506b5053082fd153dada74e683eb0c31e82be041
OKRWEBSEITE <okrschuby@gmail.com>
2026-08-05 11:05:47 +0200

    Pflegetool-Veröffentlichung: assets/daten.js

--- assets/daten.js
+++ assets/daten.js
@@ -176,15 +176,6 @@ const TERMINE = [
   },
 
   {
-    id: "OKR-2026-2-22", typ: "kurs", rubrik: "allgemein",
-    titel: "Testkurs für den Vorstand des OKR Schuby",
-    beschreibung: "Hier kann der Vorstand den Umgang mit der INternetanmeldung üben",
-    optionen: [
-      { bezeichnung: "", kursleiter: "Volker Hagge Ellhöft", start: "2026-08-12", wochentag: "Mi", uhrzeit: "1899-12-30", ort: "ONline", anzahl: 10, preis: 0, kursbegrenzung: 12 }
-    ]
-  },
-
-  {
     id: "OKR-2026-2-21", typ: "kurs", rubrik: "allgemein",
     titel: "Ipad und Iphone für Senioren",
     beschreibung: "Wir erforschen Ihre Geräte mit immer neuen Themen und Ihren mitgebrachten Themen.",
@@ -217,6 +208,15 @@ const TERMINE = [
     beschreibung: "Sie lernen den Umgang mit Ihrem eigenen Gerät",
     optionen: [
       { bezeichnung: "", kursleiter: "Matthias Alexander", start: "2026-09-14", wochentag: "Montag", uhrzeit: "1899-12-30", ort: "Schuby, Boyebüll 8", anzahl: 10, preis: 50, kursbegrenzung: 12 }
+    ]
+  },
+
+  {
+    id: "OKR-2026-2-22", typ: "kurs", rubrik: "allgemein",
+    titel: "Testkurs für den Vorstand des OKR Schuby",
+    beschreibung: "Hier kann der Vorstand den Umgang mit der INternetanmeldung üben",
+    optionen: [
+      { bezeichnung: "", kursleiter: "Volker Hagge Ellhöft", start: "2026-08-12", wochentag: "Mi", uhrzeit: "1899-12-30", ort: "ONline", anzahl: 10, preis: 0, kursbegrenzung: 12 }
     ]
   }
 ];
