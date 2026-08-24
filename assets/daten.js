3b7da65b6dd48d54a7bf7fbb4863639525c75ab3
OKRWEBSEITE <okrschuby@gmail.com>
2026-08-24 21:16:32 +0200

    Pflegetool-Veröffentlichung: assets/daten.js

--- assets/daten.js
+++ assets/daten.js
@@ -91,16 +91,6 @@ const TERMINE = [
   },
 
   {
-    id: "OKR-2026-2-17", typ: "kurs", rubrik: "koch",
-    titel: "Männer kochen",
-    beschreibung: "Gemeinsames Kochen für Männer. Hinweis: Bitte kleine Behältnisse mitbringen. Kursgebühr zzgl. Kochumlage.",
-    optionen: [
-      { bezeichnung: "Termin 1", kursleiter: "Gerd Kesseler", start: "2026-10-14", wochentag: "Mittwoch", uhrzeit: "19:00 Uhr", ort: "Schule Schuby", anzahl: 1, preis: 8, kursbegrenzung: 12 },
-      { bezeichnung: "Termin 2", kursleiter: "Gerd Kesseler", start: "2026-11-11", wochentag: "Mittwoch", uhrzeit: "19:00 Uhr", ort: "Firma Schmidt Hüsby", anzahl: 1, preis: 8, kursbegrenzung: 12 }
-    ]
-  },
-
-  {
     id: "OKR-2026-2-01", typ: "kurs", rubrik: "gesundheit",
     titel: "Wir bleiben in Bewegung mit Wassergymnastik – Dienstags-Gruppe",
     beschreibung: "Gymnastik im warmen Wasser – schonend für die Gelenke, für alle Altersgruppen geeignet.",
@@ -217,6 +207,16 @@ const TERMINE = [
       { bezeichnung: "Termin 2", kursleiter: "Kirsten Krummpeter (Kirstens Köök)", start: "2026-11-03", wochentag: "Dienstag", uhrzeit: "18:30 Uhr", ort: "Schule Schuby", anzahl: 1, preis: 8, kursbegrenzung: 12 },
       { bezeichnung: "Termin 3", kursleiter: "Kirsten Krummpeter (Kirstens Köök)", start: "2026-11-09", wochentag: "Montag", uhrzeit: "18:30 Uhr", ort: "Schule Schuby", anzahl: 1, preis: 8, kursbegrenzung: 12 },
       { bezeichnung: "Termin 4", kursleiter: "Kirsten Krummpeter (Kirstens Köök)", start: "2026-11-11", wochentag: "Mittwoch", uhrzeit: "18:30 Uhr", ort: "Schule Schuby", anzahl: 1, preis: 8, kursbegrenzung: 12 }
+    ]
+  },
+
+  {
+    id: "OKR-2026-2-17", typ: "kurs", rubrik: "koch",
+    titel: "Männer kochen",
+    beschreibung: "Gemeinsames Kochen für Männer. Hinweis: Bitte kleine Behältnisse mitbringen. Kursgebühr zzgl. Kochumlage.",
+    optionen: [
+      { bezeichnung: "Termin 1", kursleiter: "Gerd Kesseler", start: "2026-10-14", wochentag: "Mittwoch", uhrzeit: "19:00 Uhr", ort: "Schule Schuby", anzahl: 1, preis: 8, kursbegrenzung: 12 },
+      { bezeichnung: "Termin 2", kursleiter: "Gerd Kesseler", start: "2026-11-11", wochentag: "Mittwoch", uhrzeit: "19:00 Uhr", ort: "Firma Schmidt Hüsby", anzahl: 1, preis: 8, kursbegrenzung: 12 }
     ]
   }
 ];
